80f2c699a8ae9c0c32dba4dd0658b85168130945
dev <dev@A21PC12>
2024-09-05 17:20:47 +0300

    Add project files.

---
+++ c++logic.cpp
@@ -0,0 +1,77 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+
+
+
+	/*Самостійна робота 1*/
+
+	/*1
+		int number;
+		cout << "Enter the number: ";
+		cin >> number;
+
+	if (number % 2 == 0) {
+			cout << "The number is even" << endl;
+		}
+	else {
+			cout << "The number is oddе" << endl;
+		}*/
+
+	//2
+//	int num1, num2;
+//	cout << "Enter two numbers: ";
+//	cin >> num1 >> num2;
+//
+//if (num1 < num2) {
+//	cout << "Smaller number: " << num1 << endl;
+//}
+//else {
+//	cout << "Smaller number: " << num2 << endl;
+//}
+	
+	//3
+//	 int number;
+//	cout << "Enter a number: ";
+//	cin >> number;
+//
+//	if (number > 0) {
+//		cout << "Number before" << endl;
+//	}
+//	else if (number < 0) {
+//		cout << "The number is given to me" << endl;
+//	}
+//	else {
+//		cout << "The number is equal to zero" << endl;
+//	}
+// 
+	////4
+	//int num1, num2;
+	//cout << "Enter two numbers: ";
+	//cin >> num1 >> num2;
+
+	//if (num1 == num2) {
+	//cout << "Rivne numbers" << endl;
+	//}
+	//else 
+	//if (num1 < num2) {
+	//	cout << "Numbers in increments: " << num1  << num2 << endl;
+	//}
+
+	/*Самостійна робота 2*/
+//	int score, sum = 0;
+//
+//	cout << "Enter 5 student grades: " << endl;
+//
+//	for (int i = 0; i < 5; i++) {
+//		cin >> score;
+//		sum += score;
+//	} if (sum / 5.0 >= 4.0) {
+//		cout << "Student admitted to hospital" << endl;
+//	}
+//	else {
+//		cout << "The student is not allowed to go to school" << endl;
+//	}
+//}
